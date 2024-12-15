8948df66683541553e918910ffe5fd80e91593b1
Seif-Elnawawy <elnawawyseif@gmail.com>
2024-12-15 13:46:28 +0200

    add single child scroll view to book detaisl view

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -15,78 +15,81 @@ class BookDetailsView extends StatelessWidget {
       body: Padding(
         padding:
             const EdgeInsets.only(top: 30, right: 16, left: 16, bottom: 16),
-        child: Column(
-          children: [
-            const BookDetailsAppBar(),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              height: ScreenSize.screenHeight(context) / 3.3,
-              width: ScreenSize.screenWidth(context) / 2.3,
-              child: const BookImageWidget(),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              'The Jungle Book',
-              style: Styles.textStyle30,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'owner name',
-              style: Styles.textStyle18
-                  .copyWith(color: Colors.white.withOpacity(0.7)),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-                const Text(
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  '4.8',
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
-                ),
-                Text(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const BookDetailsAppBar(),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: ScreenSize.screenHeight(context) / 3.3,
+                width: ScreenSize.screenWidth(context) / 2.3,
+                child: const BookImageWidget(),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'The Jungle Book',
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'owner name',
+                style: Styles.textStyle18
+                    .copyWith(color: Colors.white.withOpacity(0.7)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.star,
+                    color: Colors.yellow,
+                  ),
+                  const Text(
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
-                    '   (2390)',
-                    style: Styles.textStyle14
-                        .copyWith(color: Colors.white.withOpacity(0.5))),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const TwoMaterialButtonsRow(),
-            const SizedBox(
-              height: 40,
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also like',
-                style: Styles.textStyle14,
+                    '4.8',
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      '   (2390)',
+                      style: Styles.textStyle14
+                          .copyWith(color: Colors.white.withOpacity(0.5))),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: ScreenSize.screenHeight(context) / 7.25,
-              child: const BuildHorizontalList(),
-            )
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              const TwoMaterialButtonsRow(),
+              const SizedBox(
+                height: 40,
+              ),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also like',
+                  style: Styles.textStyle14,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: ScreenSize.screenHeight(context) / 7.25,
+                child: const BuildHorizontalList(),
+              )
+            ],
+          ),
         ),
       ),
     );
