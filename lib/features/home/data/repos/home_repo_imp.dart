e1b4561bf200777ee792bed612afd3a08880b420
Seif-Elnawawy <elnawawyseif@gmail.com>
2024-12-16 13:00:58 +0200

    create api services class

---
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImp implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
